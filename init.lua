e273f4a3c504f94ff40faf28d48ff014d83e347a
msnadms <masonadams002@gmail.com>
2026-06-28 18:05:02 -0500

    add options

--- init.lua
+++ init.lua
@@ -16,6 +16,14 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.updatetime = 250
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
 
 -- Keybindings
 vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>w<cr>", { desc = "Save" })
